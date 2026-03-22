86b3902b29bba35a4648529c3051bff5c6245c39
NashJanaban <njanaban@ssct.edu.ph>
2026-03-22 17:54:45 +0800

    update allowed hosts

--- kopilism/kopilism/settings.py
+++ kopilism/kopilism/settings.py
@@ -4,9 +4,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-kopilism-change-this-in-production'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['nash.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
